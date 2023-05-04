fe0f14bfbb5eb38d4a2c1136828ae01e046fbcb6
DHRUVIK LATHIYA <115994798+dhruviklathiya@users.noreply.github.com>
2023-05-04 16:15:32 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -2,12 +2,14 @@
 #include <string.h>
 using namespace std;
 class A{
-	private:
+	public:
 		int emp_id;
 		int emp_salary;
 		char emp_name[20];
 		char emp_role[20];
-	public:
+        int emp_experience;
+		char emp_comp_name[20];
+		char emp_address[20];
 	void display(){
 		cout<<"Enter employee id :  ";
 		cin>>emp_id;
@@ -17,43 +19,46 @@ class A{
 		cin>>emp_role;
 		cout<<"Enter employee salary :  ";
 		cin>>emp_salary;
-		cout<<endl<<"Employee id : "<<emp_id;
-		cout<<endl<<"Employee name : "<<emp_name;
-		cout<<endl<<"Employee role in comapany : "<<emp_role;
-		cout<<endl<<"Employee salary is : "<<emp_salary<<endl;
-	}
-};
-class B{
-	private:
-		int emp_experience;
-		char emp_comp_name[20];
-		char emp_address[20];
-		char emp_email[20];
-		int emp_contact;
-	public:
-		void display(){
-		cout<<endl<<"Enter employee experience :  ";
+        cout<<"Enter employee experience :  ";
 		cin>>emp_experience;
 		cout<<"Enter employee comp_name :  ";
 		cin>>emp_comp_name;
 		cout<<"Enter employee address :  ";
 		cin>>emp_address;
+        cout<<endl;
+	}
+};
+class B{
+	public:
+		char emp_email[20];
+		int emp_contact;
+		void display(){
 		cout<<"Enter employee email :  ";
 		cin>>emp_email;
 		cout<<"Enter employee contact :  ";
 		cin>>emp_contact;
-		cout<<endl<<"Employee's experience : "<<emp_experience;
-		cout<<endl<<"Employee company name is : "<<emp_comp_name;
-		cout<<endl<<"Employee's address : "<<emp_address;
-		cout<<endl<<"Employee's email : "<<emp_email;
-		cout<<endl<<"Employee's contact : "<<emp_contact;
+		cout<<endl;
 	}
 };
 class C:public A,public B{
+        public:
+        void showinfo()
+        {
+        cout<<"Employee id : "<<emp_id<<endl;
+		cout<<"Employee name : "<<emp_name<<endl;
+		cout<<"Employee role in comapany : "<<emp_role<<endl;
+		cout<<"Employee salary is : "<<emp_salary<<endl;
+        cout<<"Employee's experience : "<<emp_experience<<endl;
+		cout<<"Employee company name is : "<<emp_comp_name<<endl;
+		cout<<"Employee's address : "<<emp_address<<endl;
+		cout<<"Employee's email : "<<emp_email<<endl;
+		cout<<"Employee's contact : "<<emp_contact<<endl;
+        }
 };
 int main(){
 	C obj;
 	obj.A::display();
 	obj.B::display();
+    obj.showinfo();
 	return 0;
 }
