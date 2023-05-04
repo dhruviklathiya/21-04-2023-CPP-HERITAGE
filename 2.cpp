74d4aaca81bae8a31216b646f5ffd2536fc76bc6
DHRUVIK LATHIYA <115994798+dhruviklathiya@users.noreply.github.com>
2023-05-04 16:04:52 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -2,32 +2,37 @@
 using namespace std;
 class animal {
 	private:
-		char name[20]="LION";
-		int animal_age = 9;
+		char name[20]="LEO";
+		int leo_age = 9;
 	public:
 	void set_value(){
-		cout<<name<<endl;
-		cout<<animal_age;
+		cout<<"Parent class ANIMAL==>"<<endl;
+        cout<<"This is LION with name: "<<name<<" and age of: "<<leo_age<<endl;
 	}
 };
 class Zebra :public animal{
 	public:
+	char zeb_name[20] = "Whitey zebra";
+	int zeb_age = 5;
 		void set_value(){
-		cout<<endl<<"THIS IS FROM ZEBRA CLASS"<<endl<<endl;
+		cout<<"Child class ZEBRA==>"<<endl;
+		cout<<"Hello this is zebra, with name of:"<<zeb_name<<"and age of:"<<zeb_age<<endl;
 	}
 };
 class Dolphin :public animal{
 	public:
-		void set_value(){
-		cout<<endl<<"THIS IS FROM DOLPHIN CLASS";
-	}
+        char dol_name[20] = "Wingy";
+        int dol_age = 7;
+	void set_value(){
+		cout<<"Child class DOLPHIN==>"<<endl;
+		cout<<"Hello this is Dolphin, with name: "<<dol_name<<"and age of:"<<dol_age<<endl;
+    }
 };
 int main(){
        Zebra obj;
        Dolphin obj1;
        obj.animal::set_value();
        obj.set_value();
-       obj1.animal::set_value();
        obj1.set_value();
 	return 0;
 }
